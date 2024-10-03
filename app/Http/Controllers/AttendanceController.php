04a9c7283d8327073bf02e90a9e4c843fc9c58ca
Zeeshan2014 <zeshanshahid2014@gmail.com>
2024-10-03 13:49:14 +0500

    dashboard.view done

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -21,7 +21,7 @@ class AttendanceController extends Controller
             ->where('user_id', $user->id)
             ->orderBy('created_at', 'desc') // Optional: order by the most recent first
             ->get();
-
+        
         // Pass the attendance records to the view
         return view('daily', [
             'attendances' => $attendances,
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
     return view('dashboard');
 }); */
 
-Route::get('/', function () {
+Route::get('/',[AttendanceController::class,'view'], function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
